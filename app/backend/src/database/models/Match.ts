26bde6193b4e076d620a5f6292c9f687fe58686a
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-28 18:30:31 -0300

    feat: Created Match model.

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,45 @@
+import { Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  public id: number;
+  public homeTeam: number;
+  public homeTeamGoals: number;
+  public awayTeam: number;
+  public awayTeamGoals: number;
+  public inProgress: number;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default Match;
