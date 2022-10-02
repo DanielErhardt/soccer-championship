e15aea7e40f6e9707fcd7a4c339220a9be7284dd
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 08:40:03 -0300

    feat: Created Validations file. There are no property validations implemented yet.

---
+++ app/backend/src/dtos/Validations.ts
@@ -0,0 +1,9 @@
+import * as Joi from 'joi';
+
+export const loginSchema = Joi.object({});
+
+export const userSchema = Joi.object({});
+
+export const teamSchema = Joi.object({});
+
+export const matchSchema = Joi.object({});
