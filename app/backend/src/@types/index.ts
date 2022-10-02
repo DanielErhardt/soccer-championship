bb3bd8cbe75099dffc6aadd71b6b66411d72a6a7
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 08:40:49 -0300

    feat: Created types for all models properties.

---
+++ app/backend/src/@types/index.ts
@@ -0,0 +1,25 @@
+export type ModelAttributes = {
+  id: number;
+};
+
+export type LoginAttributes = {
+  email: string;
+  password: string;
+};
+
+export type UserAttributes = ModelAttributes & LoginAttributes & {
+  username: string;
+  role: string;
+};
+
+export type TeamAttributes = ModelAttributes & {
+  teamName: string;
+};
+
+export type MatchAttributes = ModelAttributes & {
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: number;
+};
