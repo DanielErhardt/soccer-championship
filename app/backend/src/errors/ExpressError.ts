7136f39e59439c7f1ed9543b61c81356c13845e0
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 08:42:01 -0300

    feat: Implemented fromJoiError static method. It takes the string message from the Joi validation errors and breaks it down to have a status code and a message.

--- app/backend/src/errors/ExpressError.ts
+++ app/backend/src/errors/ExpressError.ts
@@ -1,3 +1,5 @@
+import { ValidationError } from 'joi';
+
 class ExpressError extends Error {
   protected _statusCode: number;
 
@@ -7,6 +9,11 @@ class ExpressError extends Error {
   }
 
   public get statusCode() { return this._statusCode; }
+
+  public static fromJoiError(error: ValidationError): ExpressError {
+    const [code, message] = error.details[0].message.split('|');
+    return new ExpressError(Number(code), message);
+  }
 }
 
 export default ExpressError;
