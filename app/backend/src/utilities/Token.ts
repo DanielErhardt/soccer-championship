432d1a9622686d6d2bb12adc639b855c18204915
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 08:33:35 -0300

    feat: Created Token utility file.

---
+++ app/backend/src/utilities/Token.ts
@@ -0,0 +1,20 @@
+import { sign, verify, SignOptions } from 'jsonwebtoken';
+
+class TokenService {
+  private static secret = process.env.JWT_SECRET as string;
+
+  private static options: SignOptions = {
+    algorithm: 'HS256',
+    expiresIn: '7d',
+  };
+
+  public static create(userId: number) {
+    return sign({ id: userId }, this.secret, this.options);
+  }
+
+  public static validate(token: string) {
+    return verify(token, this.secret);
+  }
+}
+
+export default TokenService;
