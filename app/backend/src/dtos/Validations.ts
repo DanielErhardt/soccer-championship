4ec6510f6b1a7db51a6288e12d53212a3a6bf4a0
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 22:26:13 -0300

    feat: Implemented Joi validation for loginSchema.

--- app/backend/src/dtos/Validations.ts
+++ app/backend/src/dtos/Validations.ts
@@ -1,6 +1,17 @@
 import * as Joi from 'joi';
 
-export const loginSchema = Joi.object({});
+const MISSING_FIELD = '400|All fields must be filled';
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.empty': MISSING_FIELD,
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.empty': MISSING_FIELD,
+  }),
+}).messages({
+  'any.required': MISSING_FIELD,
+});
 
 export const userSchema = Joi.object({});
 
