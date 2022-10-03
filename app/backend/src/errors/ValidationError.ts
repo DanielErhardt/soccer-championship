a7a5f282dcf7a246750c0ddf0a33a08be7faafca
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 22:24:16 -0300

    feat: Implemented default errors in ValidationError. The default errors are tokenNotFound and invalidToken.

--- app/backend/src/errors/ValidationError.ts
+++ app/backend/src/errors/ValidationError.ts
@@ -1,5 +1,12 @@
 import ExpressError from './ExpressError';
 
-class ValidationError extends ExpressError {}
+class ValidationError extends ExpressError {
+  public static readonly tokenNotFound = new ValidationError(401, 'Token not found');
+  public static readonly invalidToken = new ValidationError(401, 'Invalid token');
+
+  constructor(statusCode: number, message: string) {
+    super(statusCode, message, 'Validation Error');
+  }
+}
 
 export default ValidationError;
