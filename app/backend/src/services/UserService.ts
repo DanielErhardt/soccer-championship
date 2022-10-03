d1e6272d0d46d0bbc1ed46dcd568a978a4e6792e
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 22:30:23 -0300

    feat(UserService): Implemented aditional validations to login method and created getById method.

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -2,18 +2,27 @@ import ServiceError from '../errors/ServiceError';
 import Token from '../utilities/Token';
 import User from '../database/models/User';
 import LoginDTO from '../dtos/LoginDTO';
+import BCrypt from '../utilities/BCrypt';
 
 class UserService {
   private _model = User;
 
-  async login(loginDTO: LoginDTO): Promise<string> {
-    const { email } = loginDTO.getData();
+  public login = async (loginDTO: LoginDTO): Promise<string> => {
+    const { email, password } = loginDTO.getData();
     const user = await this._model.findOne({ where: { email } });
 
-    if (!user) throw new ServiceError(404, 'User not found');
+    if (!user || !BCrypt.validate(password, user.password)) {
+      throw ServiceError.incorrectLogin;
+    }
 
     return Token.create(user.id);
-  }
+  };
+
+  public getById = async (userId: number): Promise<string> => {
+    const user = await this._model.findByPk(userId);
+    if (!user) throw ServiceError.userNotFound;
+    return user.role;
+  };
 }
 
 export default UserService;
