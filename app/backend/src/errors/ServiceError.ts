7fd768de39da65366ec3d22478645512ad4d719e
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-03 16:55:05 -0300

    feat(ServiceError.ts): Implemented default error for 'Team not found.'

--- app/backend/src/errors/ServiceError.ts
+++ app/backend/src/errors/ServiceError.ts
@@ -2,7 +2,10 @@ import ExpressError from './ExpressError';
 
 class ServiceError extends ExpressError {
   public static readonly incorrectLogin = new ServiceError(401, 'Incorrect email or password');
+
   public static readonly userNotFound = new ServiceError(404, 'User not found');
+
+  public static readonly teamNotFound = new ServiceError(404, 'Team not found');
 
   constructor(statusCode: number, message: string) {
     super(statusCode, message, 'Service Error');
