9086ae47f2ac46127c9c1105c1ef9d32451cbba0
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 22:31:11 -0300

    feat(UserController): Implemented validate method.

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -9,8 +9,13 @@ class UserController {
   public login: RequestHandler = async (req, res) => {
     const loginDTO = new LoginDTO(req.body as LoginAttributes);
     const token = await this._service.login(loginDTO);
-
     return res.status(200).json({ token });
+  };
+
+  public validate: RequestHandler = async (req, res) => {
+    const { headers: { userId } } = req;
+    const role = await this._service.getById(Number(userId));
+    res.status(200).json({ role });
   };
 }
 
