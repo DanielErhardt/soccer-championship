fc77e6571367edec484ea4b9d439904d948aeb78
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-02 22:25:45 -0300

    feat: Implemented default errors in ServiceError. The errors are incorrectLogin and userNotFound.

--- app/backend/src/errors/ServiceError.ts
+++ app/backend/src/errors/ServiceError.ts
@@ -1,5 +1,12 @@
 import ExpressError from './ExpressError';
 
-class ServiceError extends ExpressError {}
+class ServiceError extends ExpressError {
+  public static readonly incorrectLogin = new ServiceError(401, 'Incorrect email or password');
+  public static readonly userNotFound = new ServiceError(404, 'User not found');
+
+  constructor(statusCode: number, message: string) {
+    super(statusCode, message, 'Service Error');
+  }
+}
 
 export default ServiceError;
