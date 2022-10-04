1556016d3ef8d494edebd77d06e98ed76cc68f86
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-03 23:43:36 -0300

    fix: Changed invalidToken error message to match the evaluator.

--- app/backend/src/errors/ValidationError.ts
+++ app/backend/src/errors/ValidationError.ts
@@ -2,7 +2,7 @@ import ExpressError from './ExpressError';
 
 class ValidationError extends ExpressError {
   public static readonly tokenNotFound = new ValidationError(401, 'Token not found');
-  public static readonly invalidToken = new ValidationError(401, 'Invalid token');
+  public static readonly invalidToken = new ValidationError(401, 'Token must be a valid token');
 
   constructor(statusCode: number, message: string) {
     super(statusCode, message, 'Validation Error');
