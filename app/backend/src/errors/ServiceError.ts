a24ea81f6302be941142f7c7f1866323d0910cb4
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-03 21:58:03 -0300

    feat(ServiceError): Created static property for match not found result.

--- app/backend/src/errors/ServiceError.ts
+++ app/backend/src/errors/ServiceError.ts
@@ -7,6 +7,8 @@ class ServiceError extends ExpressError {
 
   public static readonly teamNotFound = new ServiceError(404, 'Team not found');
 
+  public static readonly matchNotFound = new ServiceError(404, 'Match not found');
+
   constructor(statusCode: number, message: string) {
     super(statusCode, message, 'Service Error');
   }
