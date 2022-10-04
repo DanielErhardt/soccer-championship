6a420aa258193a94afc1da5be3566bfafea02f95
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-03 23:44:41 -0300

    feat: Created default error for repeated teams.

--- app/backend/src/errors/ServiceError.ts
+++ app/backend/src/errors/ServiceError.ts
@@ -9,6 +9,11 @@ class ServiceError extends ExpressError {
 
   public static readonly matchNotFound = new ServiceError(404, 'Match not found');
 
+  public static readonly repeatedTeams = new ServiceError(
+    401,
+    'It is not possible to create a match with two equal teams',
+  );
+
   constructor(statusCode: number, message: string) {
     super(statusCode, message, 'Service Error');
   }
