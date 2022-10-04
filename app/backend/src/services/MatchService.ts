bb30f2395bbe1ff0210c054aed1a3901d129e63a
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-03 23:50:59 -0300

    feat(MatchService): Implemented validations for create and setFinished methods.

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,10 +1,34 @@
+import ServiceError from '../errors/ServiceError';
+import MatchDTO from '../dtos/MatchDTO';
 import Match from '../database/models/Match';
 import MatchModel from '../models/MatchModel';
+import TeamModel from '../models/TeamModel';
 
 class MatchService {
-  private _model = new MatchModel();
+  private _matchModel = new MatchModel();
+  private _teamModel = new TeamModel();
 
-  public findAll = async (): Promise<Match[]> => this._model.findAll();
+  public findAll = async (): Promise<Match[]> => this._matchModel.findAll();
+
+  public create = async (match: MatchDTO) => {
+    const matchData = match.getData();
+    const dataTeams = [matchData.awayTeam, matchData.homeTeam];
+
+    const teams = await this._teamModel.findAllWithIds(dataTeams);
+
+    if (dataTeams[0] === dataTeams[1]) throw ServiceError.repeatedTeams;
+
+    if (teams.length !== dataTeams.length) throw ServiceError.teamNotFound;
+
+    const created = await this._matchModel.create(matchData);
+    return created;
+  };
+
+  public setFinished = async (matchId: number) => {
+    const match = await this._matchModel.findByPk(matchId);
+    if (!match) throw ServiceError.matchNotFound;
+    await this._matchModel.setFinished(matchId);
+  };
 }
 
 export default MatchService;
