00794718e35c24c2a7be2a73a3165f01413cb37a
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-03 23:51:57 -0300

    feat(MatchController): Implemented request handlers for create and setFinished methods.

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,4 +1,6 @@
 import { RequestHandler } from 'express';
+import { MatchAttributes } from '../@types';
+import MatchDTO from '../dtos/MatchDTO';
 import MatchService from '../services/MatchService';
 
 class MatchController {
@@ -10,6 +12,18 @@ class MatchController {
     res.status(200).json(inProgress
       ? matches.filter((match) => (inProgress === 'true') === match.inProgress)
       : matches);
+  };
+
+  public create: RequestHandler = async (req, res) => {
+    const match = new MatchDTO(req.body as MatchAttributes);
+    const createdMatch = await this._service.create(match);
+    res.status(201).json(createdMatch);
+  };
+
+  public setFinished: RequestHandler = async (req, res) => {
+    const { params: { id } } = req;
+    await this._service.setFinished(+id);
+    res.status(200).json({ message: 'Finished' });
   };
 }
 
