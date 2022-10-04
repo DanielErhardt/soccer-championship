89146e0b6a311127f994715dc329de87f8396d18
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-04 16:21:18 -0300

    feat: Created LeaderboardProperties type.

--- app/backend/src/@types/index.ts
+++ app/backend/src/@types/index.ts
@@ -30,3 +30,16 @@ export type MatchAttributes = ModelAttributes & MatchGoalsAttributes & {
 export type JwtPayload = {
   id: string;
 };
+
+export type LeaderboardProperties = {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: string,
+};
